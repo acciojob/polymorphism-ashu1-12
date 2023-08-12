1b44a4d1fb9025703b2d25030a32ddde72a57300
ashu1-12 <sadanshivashish122@gmail.com>
2023-08-12 16:09:12 +0530

    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,12 @@
 package com.driver;
 
-class Product{
-    public int product(int x, int y){return x+y;}
-    public  int product(int x, int y, int z){return x+y+z;}
-    public double product(double x, double y){return x+y ;}
-}
+
 class Main {
+    public static class Product{
+        public int product(int x, int y){return x*y;}
+        public  int product(int x, int y, int z){return x*y*z;}
+        public double product(double x, double y){return x*y ;}
+    }
 
     public static void main(String[] args) {
         Product p = new Product();
